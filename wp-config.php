41613b9263cd62ce93144d7aa35e60a573dbdbf6
zaiats85 <zaiats85@mail.ru>
2017-03-31 17:14:30 +0300

    installed simple lightbox plugin

--- wp-config.php
+++ wp-config.php
@@ -80,6 +80,9 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+
+define('FS_METHOD', 'direct');
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
