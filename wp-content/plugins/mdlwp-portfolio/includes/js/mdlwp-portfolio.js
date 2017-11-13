2ffdb287a498a1c5b40b9f22cbc75675b1d7ce70
zaiats85 <zaiats85@mail.ru>
2017-11-13 23:26:28 +0200

    fix styles, pagination, zoom

--- wp-content/plugins/mdlwp-portfolio/includes/js/mdlwp-portfolio.js
+++ wp-content/plugins/mdlwp-portfolio/includes/js/mdlwp-portfolio.js
@@ -10,6 +10,11 @@ jQuery(document).ready(function($){
 			loop: true,
 			hidePageListIfSinglePage: true
 		},
+		templates: {
+			pager: '<button class="mdl-button mdl-js-button ${classNames}" data-page="${pageNumber}">${pageNumber}</button>',
+			pagerPrev: '<button class="mdl-button mdl-js-button ${classNames}" data-page="prev"><i class="material-icons">keyboard_arrow_left</i></button>',
+			pagerNext: '<button class="mdl-button mdl-js-button ${classNames}" data-page="next"><i class="material-icons">keyboard_arrow_right</i></button>',
+		},
 		load: {
 			page: 1 // load page 1 on instantiation
 		},
